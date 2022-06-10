f5d68ed33c68c51bc85d0522d83fbe05063b7f16
Talha Shah <kaashugha@gmail.com>
2022-06-10 13:36:09 -0400

    Added static and templates folders and corrected references for all broken links.

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/login.html")
+def login():
+    return render_template('login.html')
+
+@app.route("/ticket.html")
+def ticket():
+    return render_template('ticket.html')
+
+@app.route("/drop-off.html")
+def dropoff():
+    return render_template('drop-off.html')
+
+@app.route("/cylinder-analysis.html")
+def cyla():
+    return render_template('cylinder-analysis.html')
+
+@app.route("/cylinder-breaking.html")
+def cylb():
+    return render_template('cylinder-breaking.html')
+
+@app.route("/create-report.html")
+def creport():
+    return render_template('create-report.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
