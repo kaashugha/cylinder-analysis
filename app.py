96995615298c89880a159195a7e28c73bd35c93a
Talha Shah <kaashugha@gmail.com>
2022-06-10 22:37:21 -0400

    initialized the sql database

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+from argparse import _UNRECOGNIZED_ARGS_ATTR
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "arrrrrrimasecretkeyarrrrrrrrr"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=14)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -17,6 +31,17 @@ def login():
         session.permanent = True
         user = request.form["username"]
         session["user"] = user
+        
+        found_user = users.query.filter_by(name=user).first()
+
+        if found_user:
+            return render_template('drop-off.html')
+        else:
+            user = users(user)
+            db.session.add(user)
+            db.session.commit()
+
+
         return redirect(url_for("user"))
     else:
         if "user" in session:
@@ -30,6 +55,11 @@ def user():
         return f"<h1>{user}</h1>"
     else:
         return redirect(url_for("login"))
+
+
+@app.route("/view/")
+def view():
+    return render_template('view.html', values=users.query.all())
 
 @app.route("/ticket/")
 def ticket():
@@ -62,4 +92,5 @@ def logout():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
