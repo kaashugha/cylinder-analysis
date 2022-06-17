38fd34c85254f21e67752f67ca4cf5eb270cf240
Talha Shah <sytalhas@gmail.com>
2022-06-16 21:09:39 -0400

    Completed the login and register pages with full control over the session and access to the database. Added a login button to the index header.

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from logging import warning
+from flask import Flask, flash, get_flashed_messages, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
-from flaskext.mysql import MySQL
- 
+import mysql.connector
+import bcrypt
+
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
 app.permanent_session_lifetime = timedelta(days=14)
+app.secret_key = 'arrrimasecretkeyarrrrrr'
 
-db = MySQL(app)
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='',
+    database='cylinders'
+)
 
-crs = db.connection.cursor()
-
-crs.close()
+crs = db.cursor(buffered=True)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -26,22 +27,21 @@ def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form["username"]
+        
+        crs.execute("SELECT password FROM user WHERE _username=%s", [user])
+        hash_password = crs.fetchone()[0]
         session["user"] = user
         
-        found_user = users.query.filter_by(name=user).first()
-
-        if found_user:
-            return render_template('drop-off.html')
+        if bcrypt.checkpw(request.form["password"].encode('utf-8'), hash_password):
+            return redirect(url_for("index"))
         else:
-            user = users(user)
-            db.session.add(user)
-            db.session.commit()
-
-
-        return redirect(url_for("user"))
+            flash("Incorrect username or password.")
+            return render_template('login.html')
+        
+        # return redirect(url_for("user"))
     else:
         if "user" in session:
-            return redirect(url_for("user"))
+            return redirect(url_for("index"))
         return render_template('login.html')
 
 @app.route("/user/")
@@ -52,14 +52,61 @@ def user():
     else:
         return redirect(url_for("login"))
 
+@app.route("/register/", methods=['POST', 'GET'])
+def register():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["newuser"]
+        password = request.form["newpass"].encode('utf-8')
+        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        
+        crs.execute("SELECT * FROM user WHERE _username=%s", [user])
+
+        if crs.rowcount == 0:
+            crs.execute("INSERT INTO user (_username, password) VALUES (%s, %s)", (user, hash_password))
+            db.commit()
+            flash(f"User {user} created.")
+        else:
+            flash("User already exists. Please select a different username.")
+
+        return redirect(url_for("register"))
+    else:
+        return render_template('register.html')
 
 @app.route("/view/")
 def view():
     return render_template('view.html', values=users.query.all())
 
-@app.route("/ticket/")
+@app.route("/ticket/", methods=['GET', 'POST'])
 def ticket():
-    return render_template('ticket.html')
+    if request.method == "POST":
+        client = request.form["client"]
+        mix = request.form["mix"]
+        ticket = request.form["ticket"]
+        address = request.form["address"]
+        load = request.form["load"]
+        agg = request.form["agg"]
+        gridlines = request.form["gridlines"]
+        spec_sl = request.form["spec_sl"]
+        conc_sup = request.form["conc_sup"]
+        toc = request.form["toc"]
+        subclient = request.form["subclient"]
+        meas_sl = request.form["meas_sl"]
+        spec_air = request.form["spec_air"]
+        cast_time = request.form["cast_time"]
+        spec_str = request.form["spec_str"]
+        mould = request.form["mould"]
+        meas_air = request.form["meas_air"]
+        conc_temp = request.form["conc_temp"]
+        cast_by = request.form["cast_by"]
+        truck_no = request.form["truck_no"]
+        amb_temp = request.form["amb_temp"]
+        min_temp = request.form["min_temp"]
+        max_temp = request.form["max_temp"]
+
+        crs.execute("INSERT INTO ticket (_batch_id) VALUES (%s)", (client,))
+    else:
+        return render_template('ticket.html')
 
 @app.route("/drop-off/")
 def dropoff():
@@ -76,10 +123,6 @@ def cylb():
 @app.route("/create-report/")
 def creport():
     return render_template('create-report.html')
-
-@app.route("/register/")
-def register():
-    return redirect(url_for("index"))
 
 @app.route("/logout/")
 def logout():
