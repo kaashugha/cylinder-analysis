64a8eadf39d1385d2932b69f24a08eb5eedd1707
Talha Shah <sytalhas@gmail.com>
2022-07-10 00:37:57 -0400

    Added user control and permissons. Admin now has power to register lab or field users who have limited access to website

--- app.py
+++ app.py
@@ -21,6 +21,31 @@ db = mysql.connector.connect(
 
 crs = db.cursor(buffered=True)
 
+def permission(user):
+    crs.execute("""SELECT role
+                FROM user
+                WHERE _username=%s
+                """, [user])
+    role = crs.fetchone()[0]
+
+
+
+    if role == 'admin':
+        return 'admin'
+    elif role == 'lab':
+        return 'lab'
+    else:
+        return 'field'
+
+def login_check():
+    if "user" in session:
+        user = session["user"]
+        return user
+    else:
+        print('out')
+        return
+
+
 
 @app.route('/sid_list/', methods=['POST'])
 def sid_list():
@@ -36,7 +61,49 @@ def sid_list():
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    if not login_check():
+        return redirect(url_for("login"))
+
+    role = permission(session['user'])
+
+    if role == 'admin':
+        return redirect(url_for('a'))
+    elif role == 'lab':
+        return redirect(url_for('l'))
+    else:
+        return redirect(url_for('f'))
+        
+@app.route("/a/", methods=["POST", "GET"])
+def a():
+    if not login_check():
+        return redirect(url_for("login"))
+
+    role = permission(session['user'])
+
+    if role == 'admin':
+        return render_template('adminindex.html')
+    else:
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
+
+
+@app.route("/l/", methods=["POST", "GET"])
+def l():
+    if not login_check():
+        return redirect(url_for("login"))
+
+    role = permission(session['user'])
+
+    if role == 'lab' or role == 'admin':
+        return render_template('labindex.html')
+    else:
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
+
+@app.route("/f/", methods=["POST", "GET"])
+def f():
+    if not login_check():
+        return redirect(url_for("login"))
+
+    return render_template('fieldindex.html')
 
 
 @app.route("/login/", methods=["POST", "GET"])
@@ -46,55 +113,69 @@ def login():
         user = request.form["username"]
 
         crs.execute("SELECT password FROM user WHERE _username=%s", [user])
-        hash_password = crs.fetchone()[0]
+        hash_password = crs.fetchone()
+        if hash_password is None:
+            flash("Incorrect username or password.")
+            return render_template('login.html')
         session["user"] = user
 
-        if bcrypt.checkpw(request.form["password"].encode('utf-8'), hash_password):
-            return redirect(url_for("index"))
+
+        if bcrypt.checkpw(request.form["password"].encode('utf-8'), *hash_password):
+            role = permission(session['user'])
+
+            if role == 'admin':
+                return redirect(url_for('a'))
+            elif role == 'lab':
+                return redirect(url_for('l'))
+            else:
+                return redirect(url_for('f'))
         else:
             flash("Incorrect username or password.")
             return render_template('login.html')
 
-        # return redirect(url_for("user"))
     else:
         if "user" in session:
-            return redirect(url_for("index"))
+            return redirect(url_for('index'))
+
         return render_template('login.html')
-
-
-@app.route("/user/")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return f"<h1>{user}</h1>"
-    else:
-        return redirect(url_for("login"))
 
 
 @app.route("/register/", methods=['POST', 'GET'])
 def register():
-    if request.method == "POST":
-        session.permanent = True
-        user = request.form["newuser"]
-        password = request.form["newpass"].encode('utf-8')
-        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    if not login_check():
+        return redirect(url_for("login"))
 
-        crs.execute("SELECT * FROM user WHERE _username=%s", [user])
+    role = permission(session['user'])
 
-        if crs.rowcount == 0:
-            crs.execute(
-                "INSERT INTO user (_username, password) VALUES (%s, %s)", (user, hash_password))
-            db.commit()
-            flash(f"User {user} created.")
+    if role == 'admin':
+        if request.method == "POST":
+            session.permanent = True
+            user = request.form["newuser"]
+            password = request.form["newpass"].encode('utf-8')
+            hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+            acc_type = request.form["acctype"]
+
+            crs.execute("SELECT * FROM user WHERE _username=%s", [user])
+
+            if crs.rowcount == 0:
+                crs.execute(
+                    "INSERT INTO user (_username, password, role) VALUES (%s, %s, %s)", (user, hash_password, acc_type))
+                db.commit()
+                flash(f"User {user} created.")
+            else:
+                flash("User already exists. Please select a different username.")
+
+            return redirect(url_for("register"))
         else:
-            flash("User already exists. Please select a different username.")
-
-        return redirect(url_for("register"))
+            return render_template('register.html')
     else:
-        return render_template('register.html')
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
 
 @app.route("/ticket/", methods=['GET', 'POST'])
 def ticket():
+    if not login_check():
+        return redirect(url_for("login"))
+
     user = session["user"]
 
     if request.method == "POST":
@@ -312,7 +393,6 @@ def ticket():
 
         return f"<h1>GREAT SUCCESS { sid }</h1>"
 
-        # return f"<h1>GREAT SUCCESS { cur_mo } and online mo: { month_online } and BID is { sid }</h1>"
     else:
         crs.execute("SELECT * FROM client")
         return render_template('ticket.html', value=crs.fetchall())
@@ -320,6 +400,9 @@ def ticket():
 
 @app.route("/drop-off/", methods=["GET", "POST"])
 def dropoff():
+    if not login_check():
+        return redirect(url_for("login"))
+
     if request.method == "POST":
         drop_id = request.form["drop_id"][2:-3]
 
@@ -344,55 +427,81 @@ def dropoff():
 
 
 @app.route("/cylinder-analysis/", methods=["GET", "POST"])
-def cyla():
-    if request.method == "POST":
+def cyla():    
+    if not login_check():
+        return redirect(url_for("login"))
 
-        sid = request.form["sid_ca"]
-        weight = request.form["weight"]
-        height = request.form["height"]
-        dia = request.form["dia"]
+    role = permission(session['user'])
 
-        crs.execute("""UPDATE Cylinder  
-            SET height = %s, weight = %s, dia = %s
-            WHERE _SID = (%s)
-            """, [height, weight, dia, sid])
-        db.commit()
+    if role == 'admin' or role == 'lab':
 
-        return "GOOD SUCCESS post"
+        if request.method == "POST":
+
+            sid = request.form["sid_ca"]
+            weight = request.form["weight"]
+            height = request.form["height"]
+            dia = request.form["dia"]
+
+            crs.execute("""UPDATE Cylinder  
+                SET height = %s, weight = %s, dia = %s
+                WHERE _SID = (%s)
+                """, [height, weight, dia, sid])
+            db.commit()
+
+            return "GOOD SUCCESS post"
+        else:
+            crs.execute(
+                "SELECT _batch_id FROM ticket ORDER BY ticket_timestamp DESC")
+            bid_cax = crs.fetchall()
+
+            return render_template('cylinder-analysis.html', bid_cax=bid_cax)
+
     else:
-        crs.execute(
-            "SELECT _batch_id FROM ticket ORDER BY ticket_timestamp DESC")
-        bid_cax = crs.fetchall()
-
-        return render_template('cylinder-analysis.html', bid_cax=bid_cax)
-        # return "GOOD SUCCESS get"
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
 
 
 @app.route("/cylinder-breaking/", methods=["GET", "POST"])
 def cylb():
-    if request.method == "POST":
-        sid = request.form["sid_ca"]
-        comp_str = request.form["comp_str"]
-        tof = request.form["tof"]
+    if not login_check():
+        return redirect(url_for("login"))
 
-        crs.execute("""UPDATE Cylinder  
-            SET comp_str = %s, frac_type = %s
-            WHERE _SID = (%s)
-            """, [comp_str, tof, sid])
-        db.commit()
+    role = permission(session['user'])
 
-        return "GOOD SUCCESS post"
+    if role == 'admin' or role == 'lab':
+        if request.method == "POST":
+            sid = request.form["sid_ca"]
+            comp_str = request.form["comp_str"]
+            tof = request.form["tof"]
+
+            crs.execute("""UPDATE Cylinder  
+                SET comp_str = %s, frac_type = %s
+                WHERE _SID = (%s)
+                """, [comp_str, tof, sid])
+            db.commit()
+
+            return "GOOD SUCCESS post"
+        else:
+            crs.execute(
+                "SELECT _batch_id FROM ticket ORDER BY ticket_timestamp DESC")
+            bid_cax = crs.fetchall()
+
+            return render_template('cylinder-breaking.html', bid_cax=bid_cax)
     else:
-        crs.execute(
-            "SELECT _batch_id FROM ticket ORDER BY ticket_timestamp DESC")
-        bid_cax = crs.fetchall()
-
-        return render_template('cylinder-breaking.html', bid_cax=bid_cax)
-
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
 
 @app.route("/create-report/")
 def creport():
-    return render_template('create-report.html')
+    if not login_check():
+        return redirect(url_for("login"))
+    
+    role = permission(session['user'])
+
+    if role == 'admin' or role == 'lab':
+        role = permission(session['user'])
+
+        return render_template('create-report.html')
+    else:
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
 
 
 @app.route("/logout/")
@@ -403,7 +512,15 @@ def logout():
 
 @app.route("/calendar/")
 def calendar():
-    return render_template('calendar.html')
+    if not login_check():
+        return redirect(url_for("login"))
+    
+    role = permission(session['user'])
+
+    if role == 'admin' or role == 'lab':
+        return render_template('calendar.html')
+    else:
+        return "<h1>Error: Insufficient Permissions to Access Page</h1>"
 
 
 if __name__ == '__main__':
